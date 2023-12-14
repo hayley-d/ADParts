594e92842aaf118a4d231202653cf29333821924
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2023-12-14 08:30:35 +0200

    Add files via upload

--- submit_form.php
+++ submit_form.php
@@ -1,4 +1,12 @@
 <?php
+
+require 'vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+use League\OAuth2\Client\Provider\Google;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
     $firstName = $_POST["name"];
@@ -30,18 +38,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message .= "Type: $radioValue\n";
     $message .= "Description: $description\n";
 
-    // Replace your-email@example.com with the actual email address where you want to receive the form data
-    $to = "your-email@example.com";
+    // Google OAuth 2.0 credentials
+    $clientId = '183845411677-ctau8mlb717s4o9s4pqfskmk1fj9ti66.apps.googleusercontent.com';
+    $clientSecret = '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';
+    $redirectUri = 'http://localhost:3000/auth/google/adParts';
 
-    // Additional headers
-    $headers = "From: $email" . "\r\n";
+    // Construct mailto URI
+    $mailtoUri = "mailto:adparts2002@gmail.com?subject=" . urlencode($subject) . "&body=" . urlencode($message);
 
-    // Send email
-    mail($to, $subject, $message, $headers);
+    /*// Create a new PHPMailer instance
+    $mail = new PHPMailer(true);
 
-    // Redirect after submission
-    header("Location: ./submit.html");
+    try {
+        // Set the mailer to use SMTP
+        $mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->Host = 'smtp.gmail.com'; // Replace with your SMTP server
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption
+        $mail->Port = 587; // TCP port to connect to
+
+        // Set Google OAuth 2.0 credentials
+        $provider = new Google([
+            'clientId' => $clientId,
+            'clientSecret' => $clientSecret,
+            'redirectUri' => $redirectUri,
+        ]);
+
+        // Fetch Gmail API token
+        $token = $provider->getAccessToken('authorization_code', [
+            'code' => 'YOUR_AUTHORIZATION_CODE',
+        ]);
+
+        // Set Gmail API token
+        $mail->setOAuth([
+            'provider' => $provider,
+            'clientId' => $clientId,
+            'clientSecret' => $clientSecret,
+            'refreshToken' => $token->getRefreshToken(),
+            'accessToken' => $token->getToken(),
+            'expires' => $token->getExpires(),
+        ]);
+
+        // Set email details
+        $mail->setFrom($email, $firstName.' '.$lastName);
+        $mail->addAddress('adparts2002@gmail.com', 'Dominic'); // Replace with the recipient's email and name
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+
+        // Send email
+        $mail->send();
+
+        echo 'Message has been sent';
+
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }*/
+    // Redirect to the mailto URI
+    header("Location: $mailtoUri");
+
     exit;
 }
-?>
+
+
 
